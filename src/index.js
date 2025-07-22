76dd8bb6a4af739b8bc1aa7b9d9cec36367e7d59
rodrigol <rodrigobernahola28@gmail.com>
2025-07-21 22:31:41 -0300

    Se agregan algunos cambios en HTML

--- src/index.js
+++ src/index.js
@@ -1,26 +1,58 @@
 import "./styles.css";
-import { greeting } from "./greeting.js";
 
-// Función de ejemplo
-const saludar = (nombre) => {
-  console.log(`¡Hola, ${nombre}!`);
-  return `¡Hola, ${nombre}!`;
-};
+function eventListener(inputElement) {
+  console.log(inputElement);
 
-// Código principal
-document.addEventListener("DOMContentLoaded", () => {
-  const app = document.getElementById("app");
-
-  if (app) {
-    app.innerHTML = "<h1>Mi aplicación con ESLint y Prettier</h1>";
-    saludar("Mundo");
+  if (inputElement.validity.valid) {
+    console.log("Cumple con requisitos");
+  } else {
+    console.log("No cumple con los requisitos");
   }
+}
+
+const inputFields = document.querySelectorAll("input");
+console.log(inputFields);
+
+inputFields.forEach((inputElement) => {
+  document.addEventListener("input", eventListener(inputElement));
 });
 
-console.log(greeting);
+// const body = document.body;
+// body.style.fontFamily = "Arial";
 
-console.log("pepe");
+// function showError() {
+//   console.log(emailInput);
+//   console.log(emailError);
+//   if (emailInput.validity.valueMissing) {
+//     console.log("poronga");
+//     emailError.textContent =
+//       "Debe introducir una dirección de correo electrónico";
+//   } else if (emailInput.validity.typeMismatch) {
+//     console.log("PORONGA");
+//     emailError.textContent =
+//       "El valor introducido debe ser una dirección de correo electrónico";
+//   }
 
-console.log("pepepepepepe");
+//   emailError.className = "error activo";
+// }
 
-export { saludar };
+// //Añadir eventlistener del tipo input a cada campo del form
+
+// const emailInput = document.querySelector("#email");
+// const emailError = document.querySelector("#email + span.error");
+// console.log(emailInput);
+// console.log(typeof emailInput);
+
+// emailInput.addEventListener("input", () => {
+//   //No se cumpln las restricciones del campo input email
+//   if (emailInput.validity.valid) {
+//     console.log("Se cumple");
+//     emailError.textContent = "";
+//     emailError.className("error");
+//   }
+//   //Se cumplen las restricciones del campo input email
+//   else {
+//     console.log("No se cumple");
+//     showError();
+//   }
+// });
